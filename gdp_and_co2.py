2465b5810fd8be54165c5802947d6f80c5d0b70e
Malcolm Smith Fraser <malcolmsfraser@gmail.com>
2020-09-29 14:59:24 -0400

    adding comments

--- gdp_and_co2.py
+++ gdp_and_co2.py
@@ -1,9 +1,9 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-# loading csv into df2
+# loading csv into df
 df = pd.read_csv('wdi_small_tidy_2015.csv')
-# extracting desired columns from df1 into df2
+# extracting desired columns from df into df2
 df2 = df[['Mortality rate, infant (per 1,000 live births)','GDP per capita (constant 2010 US$)','Country Name']]
 
 # plotting
